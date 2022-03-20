1d6b7b02ea505b043e3644e271dfe3d29b41cfc4
Rupesh <rupeshmonikaraju@zohomail.in>
2022-03-20 18:40:23 +0530

    Project added

---
+++ NimbleTest/NimbleTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  NimbleTest
+//
+//  Created by rupesh-6878 on 20/03/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ NimbleTest/NimbleTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NimbleTest
+//
+//  Created by rupesh-6878 on 20/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
